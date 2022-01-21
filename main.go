fbbb40907e41622cb8132a8b621660bbd1ab6e92
cainyao <cainyao@tencent.com>
2022-01-21 18:50:37 +0800

    base

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/lanran2001/lanrand/router"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	//mux := http.NewServeMux()
+	//mux.Handle("/", &helloHandler{})
+	muxRouter := mux.NewRouter()
+
+	router.RegisterRoutes(muxRouter)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: muxRouter,
+	}
+
+	// 创建系统信号接收器
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-done
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server:", err)
+		}
+	}()
+
+	log.Println("Starting HTTP server...")
+	err := server.ListenAndServe()
+	if err != nil {
+		if err == http.ErrServerClosed {
+			log.Print("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
+	}
+}
